2e380bf133d34647d790cfde40a6e2e58b48676e
markoco14 <76949608+markoco14@users.noreply.github.com>
2021-04-13 10:52:40 +0700

    Upload files for new Monthly/Annual feature

--- taxCalculatorScript.js
+++ taxCalculatorScript.js
@@ -72,35 +72,35 @@ let fifthTaxRate = 0.4; //40% fifth tax bracket
 let sixthTaxRate = 0.45; //45% sixth tax bracket
 
 //make max bracket amount variables
-let firstRange = 540000;
-let secondRange = 1210000 - firstRange;
-let thirdRange = 2420000 - secondRange - firstRange;
-let fourthRange = 4530000 - thirdRange - secondRange - firstRange;
-let fifthRange = 10310000 - fourthRange - thirdRange - secondRange - firstRange;
+let firstIncomeRange = 540000;
+let secondIncomeRange = 1210000 - firstIncomeRange;
+let thirdIncomeRange = 2420000 - secondIncomeRange - firstIncomeRange;
+let fourthIncomeRange = 4530000 - thirdIncomeRange - secondIncomeRange - firstIncomeRange;
+let fifthIncomeRange = 10310000 - fourthIncomeRange - thirdIncomeRange - secondIncomeRange - firstIncomeRange;
 //6th bracket open ended. to infinity and beyond. no max.
 //make an array for the income range values
 let incomeRangeArray = [
-	firstRange,
-	secondRange,
-	thirdRange,
-	fourthRange,
-	fifthRange
+	firstIncomeRange,
+	secondIncomeRange,
+	thirdIncomeRange,
+	fourthIncomeRange,
+	fifthIncomeRange
 ]
 
 //make $max tax variables for each bracket
-let firstTaxMax = Math.floor(firstRange * firstTaxRate);
-let secondTaxMax = Math.floor(secondRange * secondTaxRate);
-let thirdTaxMax = Math.floor(thirdRange * thirdTaxRate);
-let fourthTaxMax = Math.floor(fourthRange * fourthTaxRate);
-let fifthTaxMax = Math.floor(fifthRange * fifthTaxRate)
+let firstTaxRange = firstIncomeRange * firstTaxRate;
+let secondTaxRange = secondIncomeRange * secondTaxRate;
+let thirdTaxRange = thirdIncomeRange * thirdTaxRate;
+let fourthTaxRange = fourthIncomeRange * fourthTaxRate;
+let fifthTaxRange = fifthIncomeRange * fifthTaxRate;
 //6th bracket open ended. to infinity and beyond. no max.
 //make an array for the max tax variables
 let maxTaxArray = [
-	firstTaxMax,
-	secondTaxMax,
-	thirdTaxMax,
-	fourthTaxMax,
-	fifthTaxMax,
+	firstTaxRange,
+	secondTaxRange,
+	thirdTaxRange,
+	fourthTaxRange,
+	fifthTaxRange,
 ]
 //intitalize totalTax and totalIncome variables
 const totalTax = document.getElementById('total-tax')
@@ -108,6 +108,8 @@ const totalIncome = document.getElementById('total-income')
 
 // write function for income <= 540000
 let firstBracket = function() {
+	//no change necessary for the monthly/annual salary difference
+	//because I'm not using any max ranges. I'm only using the tax rate.
 	//get your gross income
 	grossIncome = input.value;
 	//find the taxes
@@ -129,24 +131,32 @@ let firstBracket = function() {
 let secondBracket = function() {
 	grossIncome = input.value;
 	//variables to separate your income into brackets
-	let grossIncomeOne = firstRange;
-	let grossIncomeTwo = grossIncome - firstRange;
+	let grossIncomeOne = firstIncomeRange;
+	let grossIncomeTwo = grossIncome - firstIncomeRange;
 	//variable to hold the taxes you pay
-	let firstIncomeTax = firstTaxMax;
+	let firstIncomeTax = firstTaxRange;
 	let secondIncomeTax = grossIncomeTwo * secondTaxRate;
 	//variables to hold the separated net incomes
-	let firstNetIncome = firstRange - firstIncomeTax;
+	let firstNetIncome = grossIncomeOne - firstIncomeTax;
 	let secondNetIncome = grossIncomeTwo - secondIncomeTax;
 	// variable to calculate the total net income
 	netIncome = firstNetIncome + secondNetIncome;
 	netTax = firstIncomeTax + secondIncomeTax;
-
-
+	
 	//set table data content
-	for (i = 0; i < 1; i++) {
-		tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
-		tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
-		/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+	//use a conditional here to correct for monthly or annual salary
+	if (monthlySalary.className === 'salary-on') {
+		for (i = 0; i < 1; i++) {
+			tableTaxArray[i].textContent = `$${(maxTaxArray[i]).toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
+	} else {
+		for (i = 0; i < 1; i++) {
+			tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
 	}
 
 	secondTableTax.textContent = `$${secondIncomeTax.toLocaleString('en-US')}`;
@@ -162,26 +172,35 @@ let secondBracket = function() {
 let thirdBracket = function() {
 	grossIncome = input.value;
 	//find the gross income for third bracket
-	let grossIncomeOne = firstRange;
-	let grossIncomeTwo = secondRange;
-	let grossIncomeThree = grossIncome - secondRange - firstRange;
+	let grossIncomeOne = firstIncomeRange;
+	let grossIncomeTwo = secondIncomeRange;
+	let grossIncomeThree = grossIncome - secondIncomeRange - firstIncomeRange;
 	//find the total taxes for third bracket
-	let firstIncomeTax = firstTaxMax;
-	let secondIncomeTax = secondTaxMax;
+	let firstIncomeTax = firstTaxRange;
+	let secondIncomeTax = secondTaxRange;
 	let thirdIncomeTax = grossIncomeThree * thirdTaxRate;
 	//find net income for third bracket
-	let firstNetIncome = firstRange - firstIncomeTax;
-	let secondNetIncome = secondRange - secondIncomeTax;
+	let firstNetIncome = grossIncomeOne - firstIncomeTax;
+	let secondNetIncome = grossIncomeTwo - secondIncomeTax;
 	let netIncomeThree = grossIncomeThree - thirdIncomeTax;
 	//find total net income
 	netIncome = netIncomeThree + firstNetIncome + secondNetIncome;
 	netTax = firstIncomeTax + secondIncomeTax + thirdIncomeTax;
 
-	// set the table data content
-	for (i = 0; i < 2; i++) {
-		tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
-		tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
-		/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+	//set table data content
+	//use a conditional here to correct for monthly or annual salary
+	if (monthlySalary.className === 'salary-on') {
+		for (i = 0; i < 2; i++) {
+			tableTaxArray[i].textContent = `$${(maxTaxArray[i]).toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
+	} else {
+		for (i = 0; i < 2; i++) {
+			tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
 	}
 
 	thirdTableTax.textContent = `$${thirdIncomeTax.toLocaleString('en-US')}`;
@@ -199,29 +218,38 @@ let thirdBracket = function() {
 let fourthBracket = function() {
 	grossIncome = input.value;
 	//establish first three brackets
-	let grossIncomeOne = firstRange;
-	let grossIncomeTwo = secondRange;
-	let grossIncomeThree = thirdRange;
-	let grossIncomeFour = grossIncome - thirdRange - secondRange - firstRange;
+	let grossIncomeOne = firstIncomeRange;
+	let grossIncomeTwo = secondIncomeRange;
+	let grossIncomeThree = thirdIncomeRange;
+	let grossIncomeFour = grossIncome - thirdIncomeRange - secondIncomeRange - firstIncomeRange;
 	//find the total taxes in each bracket
-	let firstIncomeTax = firstTaxMax;
-	let secondIncomeTax = secondTaxMax;
-	let thirdIncomeTax = thirdTaxMax;
+	let firstIncomeTax = firstTaxRange;
+	let secondIncomeTax = secondTaxRange;
+	let thirdIncomeTax = thirdTaxRange;
 	let fourthIncomeTax = grossIncomeFour * fourthTaxRate;
 	//find the net income in each bracket
-	let netIncomeOne = firstRange - firstTaxMax;
-	let netIncomeTwo = secondRange - secondTaxMax;
-	let netIncomeThree = thirdRange - thirdTaxMax;
+	let netIncomeOne = grossIncomeOne - firstIncomeTax;
+	let netIncomeTwo = grossIncomeTwo - secondIncomeTax;
+	let netIncomeThree = grossIncomeThree - thirdIncomeTax;
 	let netIncomeFour = grossIncomeFour - fourthIncomeTax;
 	//find the total net income
 	netIncome = netIncomeOne + netIncomeTwo + netIncomeThree + netIncomeFour
 	netTax = firstIncomeTax + secondIncomeTax + thirdIncomeTax + fourthIncomeTax;
 
-	// set the table data content
-	for (i = 0; i < 3; i++) {
-		tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
-		tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
-		/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+	//set table data content
+	//use a conditional here to correct for monthly or annual salary
+	if (monthlySalary.className === 'salary-on') {
+		for (i = 0; i < 3; i++) {
+			tableTaxArray[i].textContent = `$${(maxTaxArray[i]).toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${((incomeRangeArray[i]-maxTaxArray[i])).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
+	} else {
+		for (i = 0; i < 3; i++) {
+			tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
 	}
 
 	fourthTableTax.textContent = `$${fourthIncomeTax.toLocaleString('en-US')}`;
@@ -239,17 +267,17 @@ let fourthBracket = function() {
 let fifthBracket = function() {
 	grossIncome = input.value;
 	//gross income for each bracket
-	let grossIncomeOne = firstRange;
-	let grossIncomeTwo = secondRange;
-	let grossIncomeThree = thirdRange;
-	let grossIncomeFour = fourthRange;
-	let grossIncomeFive = grossIncome - fourthRange - thirdRange - secondRange - firstRange;
+	let grossIncomeOne = firstIncomeRange;
+	let grossIncomeTwo = secondIncomeRange;
+	let grossIncomeThree = thirdIncomeRange;
+	let grossIncomeFour = fourthIncomeRange;
+	let grossIncomeFive = grossIncome - fourthIncomeRange - thirdIncomeRange - secondIncomeRange - firstIncomeRange;
 
 	//tax amounts for each bracket
-	let firstIncomeTax = firstTaxMax;
-	let secondIncomeTax = secondTaxMax;
-	let thirdIncomeTax = thirdTaxMax;
-	let fourthIncomeTax = fourthTaxMax;
+	let firstIncomeTax = firstTaxRange;
+	let secondIncomeTax = secondTaxRange;
+	let thirdIncomeTax = thirdTaxRange;
+	let fourthIncomeTax = fourthTaxRange;
 	let fifthIncomeTax = grossIncomeFive * fifthTaxRate;
 
 	//net income for each bracket
@@ -263,11 +291,20 @@ let fifthBracket = function() {
 	netIncome = firstNetIncome + secondNetIncome + thirdNetIncome + fourthNetIncome + fifthNetIncome;
 	netTax = firstIncomeTax + secondIncomeTax + thirdIncomeTax + fourthIncomeTax + fifthIncomeTax;
 
-	// set the table data content
-	for (i = 0; i < 4; i++) {
-		tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
-		tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
-		/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+	//set table data content
+	//use a conditional here to correct for monthly or annual salary
+	if (monthlySalary.className === 'salary-on') {
+		for (i = 0; i < 4; i++) {
+			tableTaxArray[i].textContent = `$${(maxTaxArray[i]).toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${((incomeRangeArray[i]-maxTaxArray[i])).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
+	} else {
+		for (i = 0; i < 4; i++) {
+			tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
 	}
 
 	fifthTableTax.textContent = `$${fifthIncomeTax.toLocaleString('en-US')}`;
@@ -285,20 +322,20 @@ let sixthBracket = function() {
 	grossIncome = input.value;
 
 	//income at each bracket
-	let grossIncomeOne = firstRange;
-	let grossIncomeTwo = secondRange;
-	let grossIncomeThree = thirdRange;
-	let grossIncomeFour = fourthRange;
-	let grossIncomeFive = fifthRange;
-	let grossIncomeSix = grossIncome - fifthRange - fourthRange - thirdRange - secondRange - firstRange;
+	let grossIncomeOne = firstIncomeRange;
+	let grossIncomeTwo = secondIncomeRange;
+	let grossIncomeThree = thirdIncomeRange;
+	let grossIncomeFour = fourthIncomeRange;
+	let grossIncomeFive = fifthIncomeRange;
+	let grossIncomeSix = grossIncome - fifthIncomeRange - fourthIncomeRange - thirdIncomeRange - secondIncomeRange - firstIncomeRange;
 	
 
 	//tax at each bracket
-	let firstIncomeTax = firstTaxMax;
-	let secondIncomeTax = secondTaxMax;
-	let thirdIncomeTax = thirdTaxMax;
-	let fourthIncomeTax = fourthTaxMax;
-	let fifthIncomeTax = fifthTaxMax;
+	let firstIncomeTax = firstTaxRange;
+	let secondIncomeTax = secondTaxRange;
+	let thirdIncomeTax = thirdTaxRange;
+	let fourthIncomeTax = fourthTaxRange;
+	let fifthIncomeTax = fifthTaxRange;
 	let sixthIncomeTax = grossIncomeSix * sixthTaxRate;
 
 	//net income at each bracket
@@ -318,10 +355,20 @@ let sixthBracket = function() {
 	//but I would need variables and arrays for the calculations
 	//try to add the locale string method into the for loop
 	//since it works I can copy and paste the code into the other for loops
-	for (i = 0; i < 5; i++) {
-		tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
-		tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
-		/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+	//set table data content
+	//use a conditional here to correct for monthly or annual salary
+	if (monthlySalary.className === 'salary-on') {
+		for (i = 0; i < 5; i++) {
+			tableTaxArray[i].textContent = `$${(maxTaxArray[i]).toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${((incomeRangeArray[i]-maxTaxArray[i])).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
+	} else {
+		for (i = 0; i < 5; i++) {
+			tableTaxArray[i].textContent = `$${maxTaxArray[i].toLocaleString('en-US')}`;
+			tableIncomeArray[i].textContent = `$${(incomeRangeArray[i]-maxTaxArray[i]).toLocaleString('en-US')}`;
+			/*grossTableArray[i].textContent = `$${incomeRangeArray[i]}`;*/
+		}
 	}
 	
 	sixthTableTax.textContent = `$${sixthIncomeTax.toLocaleString('en-US')}`;
@@ -388,19 +435,43 @@ let chooseFunction = function(whichFunctions) {
 	userInput = input.value;
 	
 	grossIncome = input.value;
-	if (grossIncome < 540001) {
-		firstBracket();
-	} else if (grossIncome >= 540001  && grossIncome < 1210001) {
-		secondBracket();
-	} else if (grossIncome >= 1210001 && grossIncome < 2420001) {
-		thirdBracket();
-	} else if (grossIncome >= 2420001 && grossIncome < 4530001){
-		fourthBracket();
-	} else if (grossIncome >= 4530001 && grossIncome < 10310001) {
-		fifthBracket();
-	} else if (grossIncome >= 10310001 ) {
-		sixthBracket();
-	} 
+	//here I need to add a conditional
+	//if monthlySalary.className = 'salary-on' do the monthly way
+	//if monthlySalary.className = 'salary-off' do the annual way
+	//I think I also need to add a conditional in each of my functions.
+	//One way I could do it:
+	//check right away for the monthly class name, and change all my bracket values
+	//then let the function carry out as normal
+	if (monthlySalary.className === 'salary-on') {
+		if (grossIncome < 45001) {
+			firstBracket();
+		} else if (grossIncome >= 45001 && grossIncome < 100834) {
+			secondBracket();
+		} else if (grossIncome >= 100834 && grossIncome < 201667) {
+			thirdBracket();
+		} else if (grossIncome >= 201667 && grossIncome < 377501) {
+			fourthBracket();
+		} else if (grossIncome >= 377501 && grossIncome < 859167) {
+			fifthBracket();
+		} else if (grossIncome >= 859167) {
+			sixthBracket();
+		}
+	} else {
+		if (grossIncome < 540001) {
+			firstBracket();
+		} else if (grossIncome >= 540001  && grossIncome < 1210001) {
+			secondBracket();
+		} else if (grossIncome >= 1210001 && grossIncome < 2420001) {
+			thirdBracket();
+		} else if (grossIncome >= 2420001 && grossIncome < 4530001){
+			fourthBracket();
+		} else if (grossIncome >= 4530001 && grossIncome < 10310001) {
+			fifthBracket();
+		} else if (grossIncome >= 10310001 ) {
+			sixthBracket();
+		} 
+
+	}
 	//check if userInput is a number
 	
 	//i tried to think how i would do it..
